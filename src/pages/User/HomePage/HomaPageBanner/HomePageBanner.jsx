3ff74b181f6ce7b9dce72efa9951a3c56f347258
maitocode <quyet.doan@smartdev.com>
2022-04-21 09:15:16 +0700

    add carousel

--- src/pages/User/HomePage/HomaPageBanner/HomePageBanner.jsx
+++ src/pages/User/HomePage/HomaPageBanner/HomePageBanner.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import "./HomePageBanner.scss";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
 
 function HomePageBanner() {
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!"
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!"
+    }
+  ];
+
   return (
-    <div className="banner">
-      <h1>
-        Hello, <span></span>
-      </h1>
-      <h1>
-        My name is <span></span>
-      </h1>
-      <h1>
-        Quyet<span></span>
-      </h1>
-      <a id="hehe" href="#projects" type="button" className="cta">
-        Portfolio
-      </a>
-    </div>
+    // <div className="banner">
+    <Carousel className="banner">
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
+    // {/* </div> */}
+  );
+}
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
   );
 }
 
